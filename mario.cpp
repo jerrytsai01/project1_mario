8ad23ca9fd5f9232662dea6bb1b9041907f08f73
JonorJ <jonorjostar@gmail.com>
2024-04-19 13:12:05 +0800

    piyan

--- mario.cpp
+++ mario.cpp
@@ -9,13 +9,15 @@ mario::mario(QGraphicsPixmapItem *parent):QGraphicsPixmapItem (parent)
     landed = false;
 
     //初始位置跟圖片
-    setPos(0,450);
+    setPos(3300,450);
     setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_stand_R.png"));
 
     //持續偵測的計時
     QTimer*timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()),this,SLOT(gravity()));
     connect(timer, SIGNAL(timeout()), this, SLOT(checkKeyState()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(animation()));
+    connect(timer, SIGNAL(timeout()), this, SLOT(countY()));
+    connect(timer, SIGNAL(timeout()),this,SLOT(gravity()));
     timer->start(10);
 
 }
@@ -32,21 +34,33 @@ void mario::keyPressEvent(QKeyEvent *event)
     }
     if((event->key() == Qt::Key_Up)&&((landed == true)||y() == 450)){
         qDebug() << "jump";
-        setPos(x(),y()-200);
+        upKey = true;
+        //setPos(x(),y()-150);
     }
 }
 
 //按鍵放開事件
 void mario::keyReleaseEvent(QKeyEvent *reEvent)
 {
+    if (reEvent->isAutoRepeat()) {
+            // 如果是自动重复触发的事件，直接忽略
+            return;
+        }
     if (reEvent->key() == Qt::Key_Right) {
         rightKey = false;
-        Rtimer = 0;
+        faceRight = true;
         setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_stand_R.png"));
     }
     if (reEvent->key() == Qt::Key_Left) {
         leftKey = false;
+        faceRight = false;
         Ltimer = 0;
+        setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_stand_L.png"));
+    }
+    if (reEvent->key() == Qt::Key_Up) {
+        upKey = false;
+        UPtimer = 0;
+        velocity = 0;
         setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_stand_L.png"));
     }
 }
@@ -55,29 +69,67 @@ void mario::keyReleaseEvent(QKeyEvent *reEvent)
 void mario::checkKeyState()
 {
     if(rightKey == true){
-        setPos(x()+10,y());
-        Rtimer++;
-        if(Rtimer%10 < 5)
-            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run2_R.png"));
-        else
-            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run1_R.png"));
+        setPos(x()+2,y());
     }
     if((leftKey == true)&&(x()>0)){
-        setPos(x()-10,y());
-        Ltimer++;
-        if(Ltimer%10 < 5)
-            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run2_L.png"));
-        else
-            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run1_L.png"));
+        setPos(x()-2,y());
     }
-
+    if(upKey){
+        UPtimer++;
+        if(UPtimer <= 20){
+            velocity = -53;
+        }
+        else{
+            upKey = false;
+            UPtimer = 0;
+            //velocity = 0;
+        }
+    }
 }
 
 //給持續向下速度
 void mario::gravity(){
-    if(!landed&&y() < 450){
-        qDebug() << "1" <<endl;
-        setPos(x(),y()+5);
+    if(!landed){
+        double acceleration = 0.08; // 加速度
+        Vg += acceleration;
+    }
+}
+
+void mario::countY(){
+    Vc = Vg + velocity;
+    qDebug() <<"Vc:" << Vc << "; Vg" << Vg << "; velocity:" << velocity;
+
+    if(y()>=450&&Vc>=0){   //如果y==450且VC>=0則將速度和位置歸零
+        landed = true;
+        Vg = 0;
+        Vc = 0;
+        setPos(x(), 450);
+        qDebug() << "s";
+    }
+    if(upKey||!landed){ //按下上鍵
+        landed = false;
+        setPos(x(), y()+Vc);
+        qDebug() << "up";
+    }
+}
+
+
+void mario::animation()
+{
+    if(rightKey) {
+        Rtimer++; // 增加計數器
+        if(Rtimer % 100 < 50) { // 控制切換速度
+            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run2_R.png"));
+        } else {
+            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run1_R.png"));
+        }
+    }
+    if(leftKey){
+        Ltimer++;
+        if(Ltimer%100 < 50)
+            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run2_L.png"));
+        if(Ltimer%100 >= 50)
+            setPixmap(QPixmap(":/new/prefix1/image/Mario_small/s_mario_run1_L.png"));
     }
 }
 
--- mario.h
+++ mario.h
@@ -13,9 +13,13 @@ public:
 public slots:
     void checkKeyState();
     void gravity();
+    void animation();
+    void countY();
 private:
-    bool landed, rightKey = false, leftKey = false, upKey = false;
-    int Rtimer = 0, Ltimer = 0;
+    bool landed, rightKey = false, leftKey = false,
+        upKey = false, faceRight = 1, reachTop = false;
+    int Rtimer = 0, Ltimer = 0, UPtimer = 0;
+    double velocity = 0, Vc = 0, Vg = 0;
 };
 
 #endif // MARIO_H
