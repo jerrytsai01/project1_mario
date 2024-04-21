1b4c8b081293705ab21a08f8f514ac77450022a9
JonorJ <jonorjostar@gmail.com>
2024-04-21 22:32:33 +0800

    Floor Brick collidsion finish

--- bricks.cpp
+++
@@ -1,9 +0,0 @@
-#include "bricks.h"
-#include "QPixmap"
-#include "QGraphicsPixmapItem"
-
-bricks::bricks(const QPixmap &pixmap, int x, int y, QGraphicsItem *parent)
-    : QGraphicsPixmapItem(pixmap, parent)
-{
-    setPos(x, y); // 设置砖块位置
-}
